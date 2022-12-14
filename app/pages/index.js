f394fdb283d84c424f6c3d034b167d9702ccb2b6
victor6268 <vduvalbertin@gmail.com>
2022-12-14 16:14:51 +0100

    update: page index améliorer

--- app/pages/index.js
+++ app/pages/index.js
@@ -10,26 +10,61 @@ export default function Home() {
         <meta name="description" content="Web technologies blogging application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='wt-title'>
-        Welcome to <a href="https://www.adaltas.com">web technologies!</a>
-      </h1>
-      <ul>
-        <li>
-          <Link href="/articles">
-            View our articles
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            About us
-          </Link>
-        </li>
-        <li>
+      <div>
+ <main class="flex flex-col items-center justify-center flex-grow min-h-screen px-0 py-16" style={{ backgroundImage: `url(${"images/image1.png"})` }}>
+          <h1 class="overline text-white text-6xl m-0 absolute top-28 leading-tight">
+          Welcome to <a href="https://www.adaltas.com">web technologies!</a>
+          </h1>
+
+          <div class="flex flex-wrap items-center justify-centergrid my-0 mx-auto px-0 py-10 grid grid-cols-3 gap-4">
+
+
+            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
+              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg" src="" alt="" />
+              </a>
+              <div class="p-6">
+              <li>
           <Link href="/contact">
-            Contact us
+            A propos de nous
           </Link>
         </li>
-      </ul>
+              </div>
+            </div>
+
+
+            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
+              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg" src="" alt="" />
+              </a>
+              <div class="p-6">
+              <li>
+          <Link href="/contact">
+            Nous contacter
+          </Link>
+        </li>
+              </div>
+            </div>
+
+
+
+
+            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
+              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg" src="" alt="" />
+              </a>
+              <div class="p-6">
+              <li>
+          <Link href="/contact">
+            Nos articles
+          </Link>
+        </li>
+              </div>
+            </div>
+          </div>
+
+        </main>
+        </div>
     </Layout>
   )
 }
