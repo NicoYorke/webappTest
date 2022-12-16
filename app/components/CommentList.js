018849ecc2d15b154d313c560a1276341d553b13
NicoYorke <yorke.nicolas@gmail.com>
2022-12-17 00:27:27 +0100

    feature: CommentList component

---
+++ app/components/CommentList.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import Layout from './Layout'
+import { supabase } from '../pages/api/supabase'
+import Comment from './Comment'
+
+
+
+
+
+export default function CommentList({comments, user }){ 
+
+    
+    return (
+        <div className='py-10 px-10 min-h-screen max-w-full md:mx-auto'>
+        {comments.map(comment =>
+            <Comment comment={comment} user={user} />
+    
+          )}
+        </div>
+    )
+}
