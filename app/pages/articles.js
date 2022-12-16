8a3b02e1fc433d4e19ffebc0abd2c57a9f8eb7ed
victor6268 <vduvalbertin@gmail.com>
2022-12-16 19:25:36 +0100

    fixe: page contact us amélioré

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -63,7 +63,7 @@ export default function Articles({articles}) {
 
         </div>
         </main>
-        <MyTest ></MyTest>
+        <MyTest></MyTest>
 
         
 
@@ -76,11 +76,9 @@ function MyTest(){
   const { user, logout, loading } = useContext(UserContext)
 
   if(user){ 
-    return (
-      <main class="flex flex-col items-center justify-center flex-grow min-h-screen px-0 py-16 bg-slate-300" >
+    return (   
       <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><Link href="/addarticle">Add an article</Link></button>
-    </main>
-      )
+    )
   }
   else{ 
     return null
