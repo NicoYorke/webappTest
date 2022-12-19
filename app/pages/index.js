10441f5ada2795570b49169b8d0a5db1bfab2c75
NicoYorke <yorke.nicolas@gmail.com>
2022-12-19 21:49:28 +0100

    update: new home page

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,70 +1,55 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout.js'
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
+
 
 export default function Home() {
   return (
-    <Layout>
+    <div>
       <Head>
         <title>WebTech</title>
         <meta name="description" content="Web technologies blogging application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
       <div>
-        <main class="flex flex-col items-center justify-center flex-grow min-h-screen px-0 py-16" style={{ backgroundImage: `url(${"images/image1.png"})` }}>
-          <h1 class="overline text-white text-6xl m-0 absolute top-28 leading-tight">
-            Welcome to <a href="https://www.adaltas.com">web technologies!</a>
-          </h1>
+        
+        
 
-          <div class="flex flex-wrap items-center justify-centergrid my-0 mx-auto px-0 py-10 grid grid-cols-3 gap-4">
+        <main class="flex flex-col flex-grow min-h-screen px-0 overflow-visible">
+        
 
-
-            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
-              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="" alt="" />
+          <div className="relative text-center">
+          <img class="w-full" src="/images/computer3.jpg" alt=""/>
+          <h1 class="absolute font-extrabold tracking-tight leading-none text-gray-100 text-5xl lg:text-6xl dark:text-white text-center w-full top-1/4">Welcome to Sparticles !</h1>
+          <p class="absolute font-extrabold tracking-tight leading-none text-gray-100 text-5xl lg:text-2xl dark:text-white text-center w-full top-1/2">Give free rein to your passion and knowledge in order to share your knowledge with the whole world</p>
+          
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-3 mb-5 p-4 gap-5">
+            <div className='relative hover:scale-105 duration-50 transition ease-in-out'>
+              <a href="/articles">
+              <img className='hover:bg-black' src="/images/articles.jpg" alt=""/>
+              <h2 class="block top-1/2 absolute font-extrabold tracking-tight leading-none text-gray-100 text-5xl lg:text-6xl dark:text-white">Articles</h2>
               </a>
-              <div class="p-6">
-                <li>
-                  <Link href="/about">
-                    A propos de nous
-                  </Link>
-                </li>
-              </div>
             </div>
-
-
-            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
-              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="" alt="" />
+            <div className='relative hover:scale-105 duration-50 transition ease-in-out'>
+              <a href="/about">
+              <img src="/images/computer-using.jfif" alt=""/>
+                <h2 class="block top-1/2 absolute font-extrabold tracking-tight leading-none text-gray-100 text-5xl lg:text-6xl dark:text-white">About us</h2>
               </a>
-              <div class="p-6">
-                <li>
-                  <Link href="/contact">
-                    Nous contacter
-                  </Link>
-                </li>
-              </div>
             </div>
-
-
-
-
-            <div class="rounded-lg shadow-lg bg-red transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-100">
-              <a href="" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="" alt="" />
+            <div className='relative hover:scale-105 duration-50 transition ease-in-out'>
+              <a href="/contact">
+              <img src="/images/happy.jpg" alt=""/>
+                <h2 class="block top-1/2 absolute font-extrabold tracking-tight leading-none text-gray-100 text-5xl lg:text-6xl dark:text-white">Contact us</h2>
               </a>
-              <div class="p-6">
-                <li>
-                  <Link href="/articles">
-                    Nos articles
-                  </Link>
-                </li>
-              </div>
             </div>
           </div>
-
         </main>
+        <Footer></Footer>
       </div>
-    </Layout>
+    </div>
   )
 }
