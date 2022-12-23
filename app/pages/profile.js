97959101d491497cfdefa6a1b65b5d6d34439472
NicoYorke <yorke.nicolas@gmail.com>
2022-12-23 04:24:09 +0100

    update: minor graphics issues

--- app/pages/profile.js
+++ app/pages/profile.js
@@ -6,6 +6,7 @@ import UserContext from '../components/UserContext'
 import Gravatar from 'react-gravatar'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import ArticlesProfile from '../components/ArticlesProfile.js'
+import CommentsProfile from '../components/CommentsProfile.js'
 
 
 
@@ -132,8 +133,8 @@ if(!router.query.userID){
       <div className='flex'>
         <div className='w-1/4 bg-white p-7 rounded-xl text-center'>
             <Gravatar className="items-center inline-block rounded-lg" email={user.email} size={150}></Gravatar>
-            <p>{firstName} {lastName}</p>
-            <p className='my-3'>@{username}</p>
+            <p className='font-bold my-4'>{firstName} {lastName}</p>
+            <p className='my-3 text-blue-600'>@{username}</p>
         </div>
 
           <div className='w-3/4 mx-3 bg-white p-7 rounded-xl'>
@@ -157,7 +158,7 @@ if(!router.query.userID){
               </div>
               <div class="relative z-0 mb-2 w-full group">
                     <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
                 </div>
               
               <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
@@ -177,14 +178,12 @@ if(!router.query.userID){
           </div> 
         <div className='w-1/3 mx-3 bg-white p-3 rounded-xl text-center max-h-screen overflow-y-auto'>
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white">My articles</h1>
-
             <ArticlesProfile userID={user.id}></ArticlesProfile>
-            
         </div>
-
-          <div className='w-1/3 bg-white p-3 rounded-xl text-center'>
-          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white">My comments</h1>
-          </div>
+        <div className='w-1/3 mx-3 bg-white p-3 rounded-xl max-h-screen overflow-y-auto'>
+          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white text-center">My comments</h1>
+          <CommentsProfile userID={user.id}></CommentsProfile>
+        </div>
           
         </div>
 
@@ -249,9 +248,12 @@ if(!router.query.userID){
             
         </div>
 
-          <div className='w-1/3 bg-white p-7 rounded-xl text-center'>
-          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white">Comments</h1>
-          </div>
+        <div className='w-1/3 mx-3 bg-white p-3 rounded-xl max-h-screen overflow-y-auto'>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-2xl dark:text-white">Comments</h1>
+
+            <CommentsProfile userID={router.query.userID}></CommentsProfile>
+            
+        </div>
           
         </div>
 
